b782d48ae78addccdc7d10463f36d2b2cbe572e0
DinhXuanGiang <80635552+DinhXuanGiang@users.noreply.github.com>
2021-06-09 18:33:47 +0700

    rebuild

--- App.tsx
+++ App.tsx
@@ -3,6 +3,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
